418b7b50a2209a0779ba0bc7e77951ffb83de00e
Sam Herring <samherring111@gmail.com>
2022-07-04 15:34:58 +1000

    feat: Updated Main Text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello ServiceNow!
+          Hey Steph! 👋
         </p>
         <a
           className="App-link"
