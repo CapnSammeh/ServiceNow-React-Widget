37027e8269b9c5a73067ebc963269f867d93a3ea
Sam Herring <samherring111@gmail.com>
2022-07-04 15:49:55 +1000

    feat: :green_heart: Updated Build-No-Split to retain a standard name for the JS file

--- build-no-split.js
+++ build-no-split.js
@@ -9,3 +9,8 @@ config.optimization.splitChunks = {
 };
 
 config.optimization.runtimeChunk = false;
+
+config.output.filename = 'static/js/[name].js';
+config.output.chunkFilename = 'static/js/[name].chunk.js';
+
+config.plugins[4].filename = 'static/css[name].css';
