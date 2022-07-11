21c590c720a16365f724e749df22f705636c0533
Sam Herring <samherring111@gmail.com>
2022-07-11 10:54:34 +1000

    feat:

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Oh hey Dave!! 👋 Isn't this cool?
+          Oh hey Anthony!! 👋 Isn't this cool?
         </p>
         <a
           className="App-link"
